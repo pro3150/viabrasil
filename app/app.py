032dd16f6c7eec6ed865a85c1c530f4a3819ff20
Olavo Longo <olavo.longo@gscap.com.br>
2023-06-19 08:05:29 -0300

    links no app e casca dos htmls

--- app/app.py
+++ app/app.py
@@ -7,3 +7,34 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/index.html')
+def inicio():
+    return render_template('index.html')
+
+@app.route('/stats.html')
+def stats():
+   # Ensure the user reached path via GET
+   if request.method == "GET":
+      return render_template("stats.html")
+
+   else:
+      pass # Pass is a Python way to say 'do nothing'
+
+@app.route('/operations.html')
+def operations():
+   # Ensure the user reached path via GET
+   if request.method == "GET":
+      return render_template("operations.html")
+
+   else:
+      pass # Pass is a Python way to say 'do nothing'
+
+@app.route('/stock.html')
+def stock():
+   # Ensure the user reached path via GET
+   if request.method == "GET":
+      return render_template("stock.html")
+
+   else:
+      pass # Pass is a Python way to say 'do nothing'
