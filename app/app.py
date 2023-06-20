73b8d920cffcea8b04e565e475f15b8f2a9baa66
Lauro Cesar <lauroandradearaujo@gmail.com>
2023-06-19 21:35:52 -0300

    feat: create class DBConn

--- app/app.py
+++ app/app.py
@@ -7,3 +7,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/estoque')
+def estoque():
+    return render_template('stock.html')
---
+++ bigquery.py
@@ -0,0 +1,27 @@
+import psycopg2
+
+
+class DBConn:
+
+    def __init__(self, host, port, database, user, password):
+        self.host = host
+        self.port = port
+        self.database = database
+        self.user = user
+        self.password = password
+
+
+    def get_conn():
+        conn = psycopg2.connect(
+            host=self.host,
+            port=self.port,
+            database=self.database,
+            user=self.user,
+            password=self.password,
+        )
+        return conn
+    
+
+    
+
+    def execute_query(query: str):
